59509f375446ceee878d2bff47be31da8e900827
Lucas Molina <e1859658@cmaisonneuve.qc.ca>
2022-04-21 14:56:13 -0400

    ajuster affichage d'evenement

--- template-evenement.php
+++ template-evenement.php
@@ -1,24 +1,6 @@
-<?php 
-/**
- * Template Name: Évenement
- * 
- */
 
-?>
-
-<?php get_header() ?>
-<main class="site__main">
-   <article class="evenement">
-      <h1>-----Template-evenement-----</h1>
-      <?php if (have_posts()): the_post(); ?>
-      <?php the_title() ?>
-      <section class="evenement__resume">
-         <?php the_field('resume'); ?>
-
-      </section>
-   </article>
-   <p class="evenement__endroit">
       <?php 
+      
 /**
  * Template Name: Évenement
  * 
@@ -26,23 +8,31 @@
 
 ?>
 
-<?php get_header() ?>
-<main class="site__main">
-   <article class="evenement">
-      <h1>-----Template-evenement-----</h1>
-      <?php if (have_posts()): the_post(); ?>
-     <h1> <?php the_title() ?></h1>
-      <section class="evenement__resume">
-         <?php the_field('resume'); ?>
-      </section>
-   <p class="evenement__endroit">
-   <?php the_field('endroit'); ?>
-   </p>
-   </article>
-   <?php endif ?>
-</main>
-<?php get_footer() ?>
-   </p>
-   <?php endif ?>
-</main>
-<?php get_footer() ?>
+      <?php get_header() ?>
+      <main class="site__main">
+         <article class="evenement">
+            <h1>-----Template-evenement-----</h1>
+            <?php if (have_posts()): the_post(); ?>
+            <h1> <?php the_title() ?></h1>
+            <section class="evenement__resume">
+               <?php the_field('resume'); ?>
+            </section>
+            <p class="evenement__endroit">
+               <?php the_field('endroit'); ?>
+            </p>
+            <p> <?php the_field('organisateur'); ?> </p>
+
+
+           <?php $image = get_field('image');
+            if( !empty( $image ) ): ?>
+            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+            <?php endif; ?>
+
+         </article>
+
+
+         
+         <?php endif ?>
+      </main>
+      <?php get_footer() ?>
+  
