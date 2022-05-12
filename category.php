4634d91386665b4c780b23c2f0dec20f5790b89a
Lucas Molina <e1859658@cmaisonneuve.qc.ca>
2022-05-12 14:37:35 -0400

    s15 c1 création de github page index.html et le dossier localhost_files

--- category.php
+++ category.php
@@ -1,12 +1,16 @@
 <?php get_header() ?>
 <main class="site__main">
     <section class="formation">
+
+        <div class="fleche__ordre">
 <a href="?cletri=title&ordre=asc" class="fleche__haut">
     <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#7300ff"><path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
 </a>
 <a href="?cletri=title&ordre=desc" class="fleche__bas">
     <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#7300ff"><path fill-rule="evenodd" d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
 </a>
+</div>
+
     <?php wp_nav_menu(array(
         "menu"=>"category_cours",
         "container"=>"nav")); ?>
